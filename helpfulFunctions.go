8ffc02482b12cc270c7bd691d0bc715f40461559
BrimmStone127 <claybrimm@gmail.com>
2021-03-08 15:13:06 -0500

    Added helpfulFunctions.go

---
+++ helpfulFunctions.go
@@ -0,0 +1,17 @@
+//helpfulFunctions is a collection of useful functions that may come in handy
+package main
+
+func gcd(x, y int) int {
+	for y != 0 {
+		x, y = y, x%y
+	}
+	return x
+}
+
+func fib(n int) int {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+	}
+	return x
+}
